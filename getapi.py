b26d72bd7c56f754f281eef813f436bfbf6a8de9
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-02-23 15:42:35 +0300

    Create getapi.py

---
+++ getapi.py
@@ -0,0 +1,14 @@
+# import requests module
+import requests
+
+response  = requests.get("https://passwordinator.herokuapp.com/generate")
+# from requests.auth import HTTPBasicAuth
+# user = 63f5edb8fecdeb76b337b802
+# pass = 44bd9e69742f6240ce53f643043261db
+
+# Making a get request
+# response = requests.get('https://api.sigfox.com/v2/devices/79C030/messages/',
+			#auth = HTTPBasicAuth('user', 'pass'))
+
+# print request object
+print(response)
